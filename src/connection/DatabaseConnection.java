e520cf0c0388160863fe08c54e5f3ed52de76806
divyansh <divyanshb22@gmail.com>
2020-11-22 20:42:05 +0530

    add JavaDoc for the DatabaseConnection class

--- src/connection/DatabaseConnection.java
+++ src/connection/DatabaseConnection.java
@@ -1,4 +1,11 @@
 package connection;
 
+/**
+ * This class will create a new connection to the database.
+ * <br>
+ * This class is extremely important for JDBC implementation.
+ * <br>
+ */
 public class DatabaseConnection {
+
 }
