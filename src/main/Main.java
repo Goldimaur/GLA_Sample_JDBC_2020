eb30a29f62e1357353d7d6faa82d3817c3df6600
divyansh <divyanshb22@gmail.com>
2020-11-22 21:02:38 +0530

    attempting to connect to a database

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,7 @@ import connection.DatabaseConnection;
 public class Main {
     public static void main(String[] args) {
         DatabaseConnection databaseConnection = new DatabaseConnection();
+        // attempting to connect to a database
+        databaseConnection.connectToDatabase();
     }
 }
