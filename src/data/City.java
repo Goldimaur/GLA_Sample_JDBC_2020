af5fc7fc1e35ae13584309838d91ea0fa85d3217
divyansh <divyanshb22@gmail.com>
2020-11-22 20:31:44 +0530

    create a parameterized constructor for the City.java class

--- src/data/City.java
+++ src/data/City.java
@@ -4,4 +4,10 @@ public class City {
     private String cityName;
     private boolean isCityTraversed;
     private int kilometersRequired;
+
+    public City(String cityName, boolean isCityTraversed, int kilometersRequired) {
+        this.cityName = cityName;
+        this.isCityTraversed = isCityTraversed;
+        this.kilometersRequired = kilometersRequired;
+    }
 }
