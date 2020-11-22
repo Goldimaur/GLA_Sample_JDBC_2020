feac5c4fc4f7d04c1a1f79956040262f63d76ca9
divyansh <divyanshb22@gmail.com>
2020-11-22 21:22:07 +0530

    add JavaDoc for the CityEntryHelper.java class

--- src/helper/CityEntryHelper.java
+++ src/helper/CityEntryHelper.java
@@ -1,4 +1,16 @@
 package helper;
 
+/**
+ * This class will help us utilize the City.java data class.
+ * <br>
+ * This class will define helper methods for us to perform basic
+ * CRUD operations on the database.
+ * <br>
+ * We will create a method to:
+ * 1. Add a new entry of a City to the cities table in the database.
+ * 2. View all the cities that are present in the cities table in the database.
+ * 3. Modify the entry of a particular city in the cities table of the database.
+ * 4. Delete the entry of a particular city from the cities table of the database.
+ */
 public class CityEntryHelper {
 }
